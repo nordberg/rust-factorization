3ecafede2e7873e221c0132724d62efad2ffe54a
Marcus Nordberg <nordberg@github.com>
2015-12-23 17:18:38 +0100

    add tests to test folder

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,7 @@ fn main() {
     }
 }
 
-fn build_factor_string(n : u64) -> String {
+pub fn build_factor_string(n : u64) -> String {
 
     let mut factor_string: String = String::new();
     let mut comp = n;
@@ -109,11 +109,10 @@ fn mod_exp(base: u64, exp: u64, m: u64) -> u64 {
 
         e = e >> 1;
     }
-
     m
 }
 
-fn is_prime(p : u64) -> bool {
+pub fn is_prime(p : u64) -> bool {
     if p <= 3 {
         return true;
     }
@@ -162,35 +161,40 @@ fn is_prime(p : u64) -> bool {
     false
 }
 
-#[test]
-fn primes_under_500() {
-    let primes = vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
-        73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167,
-        173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269,
-        271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379,
-        383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487,
-        491, 499];
+#[cfg(tests)]
+mod tests {
+    use super::*;
 
-    for p in &primes {
-        println!("{}", p);
-        assert!(is_prime(*p));
+    #[test]
+    fn primes_under_500() {
+        let primes = vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+            73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167,
+            173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269,
+            271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379,
+            383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487,
+            491, 499];
+
+        for p in &primes {
+            println!("{}", p);
+            assert!(is_prime(*p));
+        }
     }
-}
 
-#[test]
-fn squares() {
-    assert_eq!(build_factor_string(9), "3^2");
-    assert_eq!(build_factor_string(25), "5^2");
-    assert_eq!(build_factor_string(64), "8^2");
-}
+    #[test]
+    fn squares() {
+        assert_eq!(build_factor_string(9), "3^2");
+        assert_eq!(build_factor_string(25), "5^2");
+        assert_eq!(build_factor_string(64), "8^2");
+    }
 
-#[test]
-fn power_of_twos() {
-    assert_eq!(build_factor_string(2), "2^1");
-    assert_eq!(build_factor_string(4), "2^2");
-    assert_eq!(build_factor_string(8), "2^3");
-    assert_eq!(build_factor_string(16), "2^4");
-    assert_eq!(build_factor_string(32), "2^5");
-    assert_eq!(build_factor_string(64), "2^6");
-    assert_eq!(build_factor_string(128), "2^7");
+    #[test]
+    fn power_of_twos() {
+        assert_eq!(build_factor_string(2), "2^1");
+        assert_eq!(build_factor_string(4), "2^2");
+        assert_eq!(build_factor_string(8), "2^3");
+        assert_eq!(build_factor_string(16), "2^4");
+        assert_eq!(build_factor_string(32), "2^5");
+        assert_eq!(build_factor_string(64), "2^6");
+        assert_eq!(build_factor_string(128), "2^7");
+    }
 }
